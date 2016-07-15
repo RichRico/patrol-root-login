76442d3589364569129dc4c663a4618f599b8f3e
RichRico <richman@mapbox.com>
2016-07-15 15:51:21 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,43 @@
+var lcfn = require ('lambda-cfn');
+var message = lcfn.message;
+
+module.exports ={};
+
+module.exports.fn = function (event, callback) {
+	
+	var notification = {
+      subject: 'Root user logged in to the console', // acepta hasta 64 caracteres
+      summary: 'Patrol detected that the root AWS user logged in to the console.',
+      event: event
+    };
+
+    
+
+    if (event.detail.userIdentity.userName == 'root'){
+    	message(notification, function(err, result) {
+      callback(err, result);
+    });
+    }
+    else {
+    	callback(null,'Not root login');
+    }
+};
+
+module.exports.config ={
+	name: 'rootLogin',
+ 	eventRule: {
+    eventPattern: {
+      'detail-type': [
+        'AWS API Call via CloudTrail'
+      ],
+      detail: {
+        eventSource: [
+          'signin.amazonaws.com'
+        ],
+        eventName: [
+          'ConsoleLogin'
+        ]
+      }
+    }
+  }
+};
